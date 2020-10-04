8b258e5f4f9bc5504d277b3ec7b98a38c04f1c69
wassimbj <wassimbenjdida@gmail.com>
2020-10-04 11:32:13 +0100

    fixed some design things

--- src/Modal.js
+++ src/Modal.js
@@ -13,12 +13,12 @@ export default function Modal({isOpen, onClose, title, isLarge, children, classN
 				<div onClick={closeModal} className="overlay close-modal" />
 				<div className={`modal modal-centered ${isLarge ? 'modal-lg' : ''}`}>
 					<div className={`modal-content shadow-lg p-5`}>
-						<div className="border-b p-2 pb-3 pt-0 mb-4">
-							<div className="flex justify-between items-center">
-								{title}
+						<div className="modal-content-header">
+							<div>
+								{!title ? <span></span> : title}
 								<span
 									onClick={closeModal}
-									className="close-modal transition cursor-pointer p-2 rounded-full bg-gray-200 hover:bg-gray-300"
+									className="close-modal"
 								>
 									<svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
 								</span>
